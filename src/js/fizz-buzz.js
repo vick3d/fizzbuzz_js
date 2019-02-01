e7f93717531d2a28bf0aff9156916257ece96e33
vick3d <viktoro0090@gmail.com>
2019-02-01 16:49:52 +0100

    refactored code

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,13 +1,20 @@
 function FizzBuzz() {
     this.check = (number) => {
-        if(number % 15 === 0) {
+        if(has_zero_remainder(number, 15)) {
             return 'FizzBuzz'
-        } else if (number % 5 === 0) {
+        } else if (has_zero_remainder(number, 5)) {
             return 'Buzz';
-        } else if(number % 3 === 0) {
+        } else if(has_zero_remainder(number, 3)) {
             return 'Fizz';
         }  else {
           return number;
         }
     }
+}
+
+function has_zero_remainder(number,divider) {
+    if(number % divider === 0) {
+        return true;
+    } else return false;
+    
 }
