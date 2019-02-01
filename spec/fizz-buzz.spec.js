d40df28ef76250fb3cbe9a72f6bdef04ee4ff496
vick3d <viktoro0090@gmail.com>
2019-02-01 15:34:02 +0100

    add new unit test, expect 3 to return Fizz

--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -8,6 +8,10 @@ describe('Fizz Buzz', () => {
         expect(fizzBuzz.check(1)).to.eql(1)
     })
 
+    it('returns Fizz if number is divisible by 3', () => {
+        expect(fizzBuzz.check(3)).to.eql('Fizz')
+    })
+
 })
 
     
